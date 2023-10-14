7cea11831a9dafd86bc68574a01d5866bacd385d
MoonKing1309 <conorken1@gmail.com>
2023-10-14 21:26:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import "react-widgets/styles.css";
+import DatePicker from "react-widgets/DatePicker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+export default function App()
+{
+  const [age,setAge] = useState(false);
+  const [temp,setTemp] = useState(false);
+
+  function handleInput(event)
+  {
+    setAge(false)
+    var today = new Date();
+    var birthDate = new Date(event.target.value.toString());
+    var agee = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      agee--
+  }
+    console.log(agee)
+   if(agee>0)
+    setTemp(`You're ${agee} years old`)
+   else if(agee<0)
+    setTemp("Invalid Input");
+   else
+    {
+      if(today.getMonth()>birthDate.getMonth())
+       {
+         setTemp(`You're ${Math.floor((today-birthDate)/(1000*60*60*24))} days old`)
+       }
+       else if((today.getMonth()<birthDate.getMonth()) && (today.getFullYear()<birthDate.getFullYear()))
+       {
+        setTemp("Invalid Input");
+       }
+       else if((today.getMonth()<=birthDate.getMonth()) && (today.getFullYear()>birthDate.getFullYear()))
+       {
+        setTemp(`You're ${Math.floor((today-birthDate)/(1000*60*60*24))} days old`)
+       }
+       else
+       {
+        if(today.getDate()>birthDate.getDate())
+        {
+          setTemp(`You're ${Math.floor((today-birthDate)/(1000*60*60*24))} days old`)
+        }
+        else if(today.getDate()<birthDate.getDate())
+        {
+         setTemp("Invalid Input");
+        }
+       }
+
+    }
+   
+   
+  }
+   function handleSubmit(event)
+  {
+    event.preventDefault();
+    setAge(temp);
+  }
+  return(
+    <div className="body">
+      <h1 className="title">Age Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date"><h5>Enter your date of birth</h5></label>
+        <div className='custome-width'>
+        <input type="date" id="date" onInput={handleInput}></input>
+        </div>
+        <button type="submit">Calculate Age</button>
+      </form>
+      <h3 className={age?"show-output":"hide-output"}>{age}</h3>
+      
+      
     </div>
-  );
+  )
 }
-
-export default App;
